72034b958536a5c51ae20343e8542c13dba613d2
Dali11 <dalitsokamphani11@gmail.com>
2024-06-17 10:46:44 -0800

    third commit

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoutes = createRouteMatcher([
-  '/api/webhook/clerk',
-  '/api/webhook/stripe',
-])
+// const isProtectedRoutes = createRouteMatcher([
+//   '/api/webhook/clerk',
+//   '/api/webhook/stripe',
+// ])
 
 export default clerkMiddleware((auth, req) => {
-  if(isProtectedRoutes(req)) auth().protect();
+  // if(isProtectedRoutes(req)) auth().protect();
 });
 
 export const config = {
